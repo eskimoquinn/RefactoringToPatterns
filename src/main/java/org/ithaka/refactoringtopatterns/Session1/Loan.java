7b355496744af3f2ae08639ef232a5d8aa06ddd2
Thomas Green <thomas.green@ithaka.org>
2017-10-11 19:29:21 -0400

    notes

--- src/main/java/org/ithaka/refactoringtopatterns/Session1/Loan.java
+++ src/main/java/org/ithaka/refactoringtopatterns/Session1/Loan.java
@@ -11,21 +11,23 @@ public class Loan {
     private Date expiry;
     private CapitalStrategy capitalStrategy;
 
-    /*
-    A term loan is a loan that must be fully paid by its maturity date.
-    A revolver, which is like a credit card, is a loan that signifies “revolving credit”:
-    you have a spending limit and an expiry date.
-    A revolving credit term loan (RCTL) is a revolver that transforms into a term loan when the revolver expires
-    */
-
     public Loan(double commitment, int riskRating, Date maturity) {
         this(commitment, 0.00, riskRating, maturity, null);
+    }
+
+    public Loan(double commitment, int riskRating, Date maturity, Date expiry) {
+        this(commitment, 0.00, riskRating, maturity, expiry);
     }
 
     public Loan(double commitment, double outstanding, int riskRating, Date maturity, Date expiry) {
         this(null, commitment, outstanding, riskRating, maturity, expiry);
     }
 
+    public Loan(CapitalStrategy capitalStrategy, double commitment, int riskRating, Date maturity, Date expiry) {
+        this(capitalStrategy, commitment, 0.00, riskRating, maturity, expiry);
+    }
+
+    //catch-all
     public Loan(CapitalStrategy capitalStrategy, double commitment, double outstanding, int riskRating,
                 Date maturity, Date expiry) {
         this.commitment = commitment;
@@ -46,11 +48,5 @@ public class Loan {
         }
     }
 
-    public Loan(double commitment, int riskRating, Date maturity, Date expiry) {
-        this(commitment, 0.00, riskRating, maturity, expiry);
-    }
 
-    public Loan(CapitalStrategy capitalStrategy, double commitment, int riskRating, Date maturity, Date expiry) {
-        this(capitalStrategy, commitment, 0.00, riskRating, maturity, expiry);
-    }
 }
