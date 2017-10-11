ab8836b8bf18ab6bb6490810bb0eede301231a4c
Thomas Green <thomas.green@ithaka.org>
2017-10-11 09:57:01 -0400

    initial phase

--- src/main/java/org/ithaka/refactoringtopatterns/Session1/Loan.java
+++ src/main/java/org/ithaka/refactoringtopatterns/Session1/Loan.java
@@ -3,58 +3,30 @@ package org.ithaka.refactoringtopatterns.Session1;
 import java.util.Date;
 
 public class Loan {
-    private final double commitment;
+
+    private double commitment;
     private double outstanding;
-    private final int riskRating;
-    private final Date maturity;
-    private final Date expiry;
+    private int riskRating;
+    private Date maturity;
+    private Date expiry;
     private CapitalStrategy capitalStrategy;
 
-
     /*
-    These constructors make it hard to decide which constructor to use. They also obscure important business rules.
-    These types of classes also leads to dead constructors that are no longer used.
+    A term loan is a loan that must be fully paid by its maturity date.
+    A revolver, which is like a credit card, is a loan that signifies “revolving credit”:
+    you have a spending limit and an expiry date.
+    A revolving credit term loan (RCTL) is a revolver that transforms into a term loan when the revolver expires
+    */
 
-    +  Communicates what kinds of instances are available better than constructors.
-    +  Bypasses constructor limitations, such as the inability to have two constructors with
-     the same number and type of arguments.
-     +  Makes it easier to find unused creation code.
-
-
-    Let's replace these constructors with intention-revealing Creation Methods
-
-    Potential Negatives:
-    - May introduce non-standard ways to create object. Must decide if its worth the tradeoff.
-     */
-
-    //A term loan is a loan that must be fully paid by its maturity date.
-    public static Loan createTermLoan(double commitment, int riskRating, Date maturity) {
-        return new Loan(null, commitment, 0.00, riskRating, maturity, null);
+    public Loan(double commitment, int riskRating, Date maturity) {
+        this(commitment, 0.00, riskRating, maturity, null);
     }
 
-    //A revolver, which is like a credit card, is a loan that signifies “revolving credit”:
-    // you have a spending limit and an expiry date.
-    static Loan createRevolverLoan(double commitment, int riskRating, Date expiry) {
-        return new Loan(null, commitment, 0.00, riskRating, null, expiry);
+    public Loan(double commitment, double outstanding, int riskRating, Date maturity, Date expiry) {
+        this(null, commitment, outstanding, riskRating, maturity, expiry);
     }
 
-    //A revolving credit term loan (RCTL) is a revolver that transforms into a term loan when the revolver expires
-    static Loan createRctlLoan(double commitment, double outstanding, int riskRating, Date maturity, Date expiry) {
-        return new Loan(null, commitment, outstanding, riskRating, maturity, expiry);
-    }
-
-    static Loan createRctlLoan(CapitalStrategy captialStrategyRCTL, double commitment, int riskRating, Date maturity, Date expiry) {
-        return new Loan(captialStrategyRCTL, commitment, 0.00, riskRating, maturity, expiry);
-    }
-
-    public static Loan createTermLoan(CapitalStrategy riskAdjustedCapitalStrategy, double commitment, double outstanding, int riskRating, Date maturity) {
-        return new Loan(riskAdjustedCapitalStrategy, commitment,
-                outstanding, riskRating, maturity, null);
-    }
-
-    //Catch all constructor
-    private Loan(CapitalStrategy capitalStrategy, double commitment,
-                double outstanding, int riskRating,
+    public Loan(CapitalStrategy capitalStrategy, double commitment, double outstanding, int riskRating,
                 Date maturity, Date expiry) {
         this.commitment = commitment;
         this.outstanding = outstanding;
@@ -64,55 +36,21 @@ public class Loan {
         this.capitalStrategy = capitalStrategy;
 
         if (capitalStrategy == null) {
-            if (expiry == null)
+            if (expiry == null) {
                 this.capitalStrategy = new CapitalStrategyTermLoan();
-            else if (maturity == null)
+            } else if (maturity == null) {
                 this.capitalStrategy = new CapitalStrategyRevolver();
-            else
+            } else {
                 this.capitalStrategy = new CapitalStrategyRCTL();
+            }
         }
     }
 
-
-
-
-    public void makePayment(double payment) {
-        outstanding -= payment;
+    public Loan(double commitment, int riskRating, Date maturity, Date expiry) {
+        this(commitment, 0.00, riskRating, maturity, expiry);
     }
 
-    public double getCommitment() {
-        return commitment;
+    public Loan(CapitalStrategy capitalStrategy, double commitment, int riskRating, Date maturity, Date expiry) {
+        this(capitalStrategy, commitment, 0.00, riskRating, maturity, expiry);
     }
-
-    public double getOutstanding() {
-        return outstanding;
-    }
-
-    public int getRiskRating() {
-        return riskRating;
-    }
-
-    public Date getMaturity() {
-        return maturity;
-    }
-
-    public Date getExpiry() {
-        return expiry;
-    }
-
-    public CapitalStrategy getCapitalStrategy() {
-        return capitalStrategy;
-    }
-
-    public Loan setCapitalStrategy(CapitalStrategy capitalStrategy) {
-        this.capitalStrategy = capitalStrategy;
-        return this;
-    }
-
-    public Loan setOutstanding(double outstanding) {
-        this.outstanding = outstanding;
-        return this;
-    }
-
-
 }
--- src/test/java/org/ithaka/refactoringtopatterns/Session1/CapitalCalculationTest.java
+++ src/test/java/org/ithaka/refactoringtopatterns/Session1/CapitalCalculationTest.java
@@ -1,15 +1,10 @@
 package org.ithaka.refactoringtopatterns.Session1;
 
 import org.junit.Before;
-import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.util.Date;
-
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
 
 public class CapitalCalculationTest {
 
@@ -32,63 +27,5 @@ public class CapitalCalculationTest {
         captialStrategyRCTL = new CapitalStrategyRCTL();
     }
 
-    @Test
-    public void testTermLoanNoPayments() throws Exception {
-
-        Loan termLoan = Loan.createTermLoan(commitment, riskRating, maturity);
-
-        assertThat(termLoan.getCommitment() , is(commitment));
-        assertThat(termLoan.getRiskRating() , is(riskRating));
-        assertThat(termLoan.getMaturity() , is(maturity));
-        assertThat(termLoan.getOutstanding() , is(0.00));
-        assertThat(termLoan.getExpiry() , nullValue());
-    }
-
-    @Test
-    public void testTermLoanOnePayment() throws Exception {
-        Loan termLoan = Loan.createTermLoan(commitment, riskRating, maturity);
-        termLoan.setOutstanding(outstanding);
-        termLoan.makePayment(3.00);
-
-        assertThat(termLoan.getOutstanding(), is(17.0));
-
-    }
-
-    @Test
-    public void testTermLoanWithRiskAdjustedCapitalStrategy() {
-        Loan termLoan = Loan.createTermLoan(riskAdjustedCapitalStrategy, commitment, outstanding, riskRating, maturity);
-        //Null in a constructor, bad practice!
-
-        assertThat(termLoan.getCommitment() , is(commitment));
-        assertThat(termLoan.getRiskRating() , is(riskRating));
-        assertThat(termLoan.getMaturity() , is(maturity));
-        assertThat(termLoan.getOutstanding() , is(outstanding));
-        assertThat(termLoan.getExpiry() , nullValue());
-    }
-
-    @Test
-    public void testRevolver() throws Exception {
-        Loan revolverLoan = Loan.createRevolverLoan(commitment, riskRating, expiry);
-
-        assertThat(revolverLoan.getCommitment(), is(commitment));
-        assertThat(revolverLoan.getMaturity(), nullValue());
-    }
-
-    @Test
-    public void testRctl(){
-        Loan revolverLoan = Loan.createRctlLoan(commitment, outstanding, riskRating, maturity, expiry);
-
-        assertThat(revolverLoan.getMaturity(), is(maturity));
-        assertThat(revolverLoan.getExpiry(), is(expiry));
-    }
-
-    @Test
-    public void testRctlWithCapitalStrategy(){
-        Loan revolverLoan = Loan.createRctlLoan(captialStrategyRCTL, commitment, riskRating, maturity, expiry);
-
-        assertThat(revolverLoan.getMaturity(), is(maturity));
-        assertThat(revolverLoan.getExpiry(), is(expiry));
-        assertThat(revolverLoan.getOutstanding(), is(0.00));
-    }
 
 }
